382efd881300574e67b3d43a71b1253be89af403
hadzicni <nikola.hadzic@usb.ch>
2024-09-12 16:10:20 +0200

    fix speedtest syntax

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import speedtest
 
-test = speedtest.Speedtest()
-download = test.download()
-upload = test.upload()
+def run_speedtest():
+    st = speedtest.Speedtest()
 
-print(f"Download Speed: {download}")
-print(f"Upload Speed: {upload}")
+    print("Choose the best server...")
+    st.get_best_server()
+
+    print("Test Download-Speed...")
+    download_speed = st.download() / 1_000_000 
+    print("Test Upload-Speed...")
+    upload_speed = st.upload() / 1_000_000 
+    ping = st.results.ping
+
+    print(f"Ping: {ping} ms")
+    print(f"Download: {download_speed:.3f} Mbit/s")
+    print(f"Upload: {upload_speed:.3f} Mbit/s")
+
+if __name__ == "__main__":
+    run_speedtest()
